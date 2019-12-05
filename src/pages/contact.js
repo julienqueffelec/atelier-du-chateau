19d08347844eaa58ebd9cc5488b89b043c07f0cd
Julien Queffelec <julienqueffelec@macbook-pro-de-julien.home>
2019-12-05 20:03:03 +0100

    fix contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,89 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import { navigateTo } from 'gatsby-link';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import MapContainer from '../components/map';
 
-const Contact = () => (
-	<Layout>
-		<SEO title="Contact" />
-		<form name="contact" method="POST" data-netlify="true">
-			<p>
-				<label>
-					Your Name: <input type="text" name="name" />
-				</label>
-			</p>
-			<p>
-				<label>
-					Your Email: <input type="email" name="email" />
-				</label>
-			</p>
-			<p>
-				<label>
-					Message: <textarea name="message"></textarea>
-				</label>
-			</p>
-			<p>
-				<button type="submit">Send</button>
-			</p>
-		</form>
-		<div style={{ width: '600px', height: '300px', position: 'relative' }}>
-			<MapContainer></MapContainer>
-		</div>
-	</Layout>
-);
+function encode(data) {
+	return Object.keys(data)
+		.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+		.join('&');
+}
 
-export default Contact;
+export default class Contact extends React.Component {
+	handleSubmit = e => {
+		e.preventDefault();
+		const form = e.target;
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: encode({
+				'form-name': form.getAttribute('name'),
+				...this.state
+			})
+		})
+			.then(() => navigateTo(form.getAttribute('action')))
+			.catch(error => alert(error));
+	};
+
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	render() {
+		return (
+			<Layout>
+				<SEO title="Contact" />
+				<form
+					name="contact"
+					method="post"
+					action="/thanks/"
+					data-netlify="true"
+					data-netlify-honeypot="bot-field"
+					onSubmit={this.handleSubmit}
+				>
+					{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+					<input type="hidden" name="form-name" value="contact" />
+					<p hidden>
+						<label>
+							Don’t fill this out:{' '}
+							<input name="bot-field" onChange={this.handleChange} />
+						</label>
+					</p>
+					<p>
+						<label>
+							Your name:
+							<br />
+							<input type="text" name="name" onChange={this.handleChange} />
+						</label>
+					</p>
+					<p>
+						<label>
+							Your email:
+							<br />
+							<input
+								type="email"
+								name="email"
+								onChange={this.handleChange}
+							/>
+						</label>
+					</p>
+					<p>
+						<label>
+							Message:
+							<br />
+							<textarea name="message" onChange={this.handleChange} />
+						</label>
+					</p>
+					<p>
+						<button type="submit">Send</button>
+					</p>
+				</form>
+				<div style={{ width: '600px', height: '300px', position: 'relative' }}>
+					<MapContainer></MapContainer>
+				</div>
+			</Layout>
+		);
+	}
+}
---
+++ src/pages/thanks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+
+const Thanks = () => (
+	<Layout>
+		<SEO title="404: Not found" />
+		<h1>Thanks</h1>
+	</Layout>
+);
+
+export default Thanks;
